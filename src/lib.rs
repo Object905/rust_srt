51ca0f505f9617214f4778cf9eccf732756c966a
Object905 <object905@gmail.com>
2016-07-02 10:40:48 +0000

    some basic edit functionlity

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
-#[macro_use]
-extern crate lazy_static;
+#[macro_use] extern crate lazy_static;
 extern crate regex;
 
 use regex::Regex;
@@ -30,7 +29,7 @@ fn read_file(path: &Path) -> Result<String> {
     Ok(content)
 }
 
-fn parse(content: String) -> Subtitles {
+fn parse(content: String, path: &str) -> Subtitles {
     let mut result: Vec<SubLine> = vec![];
 
     for cap in SUBS.captures_iter(&content) {
@@ -48,15 +47,13 @@ fn parse(content: String) -> Subtitles {
 
         let line = SubLine {
             index: cap.at(1).unwrap().parse::<u32>().unwrap(),
-            start_timestamp: start_timestamp,
-            end_timestamp: end_timestamp,
             text: cap.at(12).unwrap().to_string(),
             start: start,
             end: end,
         };
         result.push(line)
     }
-    Subtitles { field: result }
+    Subtitles { field: result, path: path.to_string() }
 }
 
 
@@ -69,12 +66,18 @@ fn to_miliseconds(timestamp: &[u32; 4]) -> u32 {
     result
 }
 
+fn recover_timestamp(miliseconds: &u32) -> [u32;4] {
+    let hours = miliseconds / 3600000;
+    let minutes = (miliseconds - hours * 3600000) / 60000;
+    let seconds = (miliseconds - (hours * 3600000 + minutes * 60000)) / 1000;
+    let miliseconds = miliseconds - (hours * 3600000 + minutes * 60000 + seconds * 1000);
+    [hours, minutes, seconds, miliseconds]
+}
+
 #[derive(Debug, Clone)]
 pub struct SubLine {
     text: String,
     index: u32,
-    start_timestamp: [u32; 4],
-    end_timestamp: [u32; 4],
     start: u32,
     end: u32,
 }
@@ -84,36 +87,65 @@ impl fmt::Display for SubLine {
         write!(f,
                "{}\n{:?} --> {:?} ({}>>{})\n{}",
                self.index,
-               self.start_timestamp,
-               self.end_timestamp,
+               self.start,
+               self.end,
                self.start,
                self.end,
                self.text)
     }
 }
 
-
 impl SubLine {
-    fn get_start(&self) -> u32 {
+    pub fn get_start(&self) -> u32 {
         self.start
     }
 
-    fn get_start_timestamp(&self) -> [u32; 4] {
-        self.start_timestamp
+    pub fn get_text(&self) -> String {
+        self.text.clone()
     }
 
-    fn get_end(&self) -> u32 {
+    pub fn get_start_timestamp(&self) -> [u32; 4] {
+        recover_timestamp(&self.start)
+    }
+
+    pub fn get_end_timestamp(&self) -> [u32; 4] {
+        recover_timestamp(&self.end)
+    }
+
+    pub fn get_index(&self) -> u32 {
+        self.index
+    }
+
+    pub fn get_end(&self) -> u32 {
         self.end
     }
 
-    fn get_duration(&self) -> u32 {
+    pub fn get_duration(&self) -> u32 {
         self.end - self.start
+    }
+
+    pub fn shift(&mut self, offset: i32) -> Result<()> {
+        if offset > 0 {
+            self.start += offset as u32;
+            self.end += offset as u32;
+            Ok(())
+        } else if offset < 0 {
+            if (-offset as u32 >= self.start) || (-offset as u32 >= self.end) {
+                return Err("start can't be negative".to_string())
+            };
+            self.start -= -offset as u32;
+            self.end -= -offset as u32;
+            Ok(())
+        } else {
+            Err("offset can't be 0".to_string())
+        }
     }
 }
 
 #[derive(Debug)]
 pub struct Subtitles {
     field: Vec<SubLine>,
+    path: String,
 }
 
 impl fmt::Display for Subtitles {
@@ -126,28 +158,41 @@ impl fmt::Display for Subtitles {
     }
 }
 
+impl IntoIterator for Subtitles {
+    type Item = SubLine;
+    type IntoIter = ::std::vec::IntoIter<SubLine>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.field.into_iter()
+    }
+}
+
 impl Subtitles {
-    fn at_index(&self, index: usize) -> Option<SubLine> {
+    pub fn get_path(&self) -> String {
+        self.path.clone()
+    }
+
+    pub fn by_index(&self, index: usize) -> Option<SubLine> {
         self.field.get(index - 1).map(|n| n.clone())
     }
 
-    fn get_length(&self) -> usize {
+    pub fn get_length(&self) -> usize {
         self.field.len()
     }
 
-    fn by_timestamp(&self, timestamp: [u32; 4]) -> Option<SubLine> {
+    pub fn by_timestamp(&self, timestamp: &[u32; 4]) -> Option<SubLine> {
         let miliseconds = to_miliseconds(&timestamp);
         self.by_miliseconds(miliseconds)
     }
 
-    fn by_miliseconds(&self, time: u32) -> Option<SubLine> {
+    pub fn by_miliseconds(&self, time: u32) -> Option<SubLine> {
+        // binary search
         let mut min = 0;
         let mut max = self.field.len() - 1;
         let mut guess_index;
 
         while max >= min {
             guess_index = (max + min) / 2;
-            let guess = &self.field[guess_index];
+            let guess = self.field.get(guess_index).unwrap();
 
             if (guess.start <= time) && (time <= guess.end) {
                 return Some(guess.clone());
@@ -159,7 +204,54 @@ impl Subtitles {
         }
         None
     }
+
+    pub fn mut_by_miliseconds(&mut self, time: u32) -> Option<&mut SubLine> {
+        // binary search
+        let mut min = 0;
+        let mut max = self.field.len() - 1;
+        let mut guess_index;
+        let mut result: usize = 0;
+        let mut state = false;
+
+        while max >= min {
+            guess_index = (max + min) / 2;
+
+            let guess = self.field.get(guess_index).unwrap();
+
+            if (guess.start <= time) && (time <= guess.end) {
+                result = guess_index;
+                state = true;
+                break;
+            } else if time > guess.end {
+                min = guess_index + 1;
+            } else {
+                max = guess_index - 1;
+            }
+        }
+
+        if state {
+            Some(&mut self.field[result])
+        } else { None }
+    }
+
+    pub fn mut_by_timestamp(&mut self, timestamp: &[u32; 4]) -> Option<&mut SubLine> {
+        let miliseconds = to_miliseconds(&timestamp);
+        self.mut_by_miliseconds(miliseconds)
+    }
+
+    pub fn mut_by_index(&mut self, index: usize) -> Option<&mut SubLine>{
+        self.field.get_mut(index - 1)
+    }
+
+    pub fn shift_all(&mut self, offset: i32) -> Result<()> {
+        for line in &mut self.field {
+            try!(line.shift(offset));
+        }
+        Ok(())
+    }
 }
+
+
 
 pub fn prepare(path: &str) -> Result<Subtitles> {
     let sub_path = Path::new(path);
@@ -178,16 +270,44 @@ pub fn prepare(path: &str) -> Result<Subtitles> {
         return Err("Bad srt file".to_string());
     }
 
-    Ok(parse(content))
+    Ok(parse(content, path))
 }
-
 
 #[test]
 fn it_works() {
     let path = "ex1";
-    let subs = prepare(path).unwrap();
+    let mut subs = prepare(path).unwrap();
 
-    println!("{}", subs.at_index(3).unwrap());
-    println!("{}", subs.by_miliseconds(261072).unwrap());
-    println!("{}", subs.by_timestamp([0, 4, 22, 500]).unwrap());
+    let line = subs.by_index(3).unwrap();
+    {
+        let another_one = subs.by_miliseconds(261072).unwrap();
+        let same_line = subs.by_timestamp(&[0, 4, 22, 500]).unwrap();
+        assert_eq!(line.get_index(), same_line.get_index());
+        assert_eq!(same_line.get_text(), another_one.get_text());
+    }
+
+    println!("{}\n{:?} --> {:?}\n{}\n",
+             line.get_index(),
+             line.get_start(),
+             line.get_end(),
+             line.get_text());
+    println!("duration: {}", line.get_duration());
+
+    {
+        let mut mutline = subs.mut_by_miliseconds(261072).unwrap();
+        mutline.shift(-1).unwrap();
+    }
+
+    {
+        let newline = subs.by_index(3).unwrap();
+        assert_eq!(newline.get_start(), line.get_start() - 1);
+    }
+    
+    subs.shift_all(1).unwrap();
+
+    {
+        let newline = subs.by_index(3).unwrap();
+        assert_eq!(newline.get_start(), line.get_start());
+    }
+
 }
